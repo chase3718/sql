195d711e7537330dae42e9f14d75393315bb7379
Chase <chasewilliams98@gmail.com>
2019-04-16 15:43:31 -0400

    varchar to nvarchar

--- SqlBootcampExample.sql
+++ SqlBootcampExample.sql
@@ -3,12 +3,12 @@ CREATE DATABASE SqlBootcampExample;
 use SqlBootcampExample;
 
 CREATE TABLE Customer (
-	ID int not null primary key auto_increment,
-	Name varchar(50) not null,
-	City varchar(50) not null,
-	State varchar(2) not null,
-	Sales decimal(18,0) not null,
-	Active bit not null
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Name NVARCHAR(50) NOT NULL,
+    City NVARCHAR(50) NOT NULL,
+    State NVARCHAR(2) NOT NULL,
+    Sales DECIMAL(20, 2) NOT NULL,
+    Active BIT NOT NULL
 );
 
 Insert Customer (Name, City, State, Sales, Active) Values ('Acme, inc.','Jersey City','NJ',14381891,1);
@@ -21,3 +21,6 @@ Insert Customer (Name, City, State, Sales, Active) Values ('ABC Telecom','Birmin
 Insert Customer (Name, City, State, Sales, Active) Values ('Fake Brothers','Stockton','CA',31226191,1);
 Insert Customer (Name, City, State, Sales, Active) Values ('QWERTY Logistics','Lubbock','TX',17226905,1);
 Insert Customer (Name, City, State, Sales, Active) Values ('Demo, Inc.','Las Vegas','NV',69551557,1);
+Insert into Customer (Name, City, State, Sales, Active) Values 
+('Inconspicuous Company','Cincinnati','OH',999999999999999999.99,0),
+('Unassuming Inc','Lexington','KY',3.50,1);
