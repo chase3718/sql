f2b56ba00efdcc88b0e14db01cc7e1e3da34c0f4
Chase <chasewilliams98@gmail.com>
2019-04-16 16:05:17 -0400

    changed values

--- SqlBootcampExample.sql
+++ SqlBootcampExample.sql
@@ -11,16 +11,16 @@ CREATE TABLE Customer (
     Active BIT NOT NULL
 );
 
-Insert Customer (Name, City, State, Sales, Active) Values ('Acme, inc.','Jersey City','NJ',14381891,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('Widget Corp','Seattle','WA',97865829,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('123 Warehousing','Los Angeles','CA',77503710,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('Demo Company','Columbus','OH',74647541,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('Smith and Co.','Honolulu','HI',36046590,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('Foo Bars','Fresno','CA',39912857,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('ABC Telecom','Birmingham','AL',31149083,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('Fake Brothers','Stockton','CA',31226191,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('QWERTY Logistics','Lubbock','TX',17226905,1);
-Insert Customer (Name, City, State, Sales, Active) Values ('Demo, Inc.','Las Vegas','NV',69551557,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Acme, inc.','Jersey City','NJ',143818.91,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Widget Corp','Seattle','WA',978658.29,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('123 Warehousing','Los Angeles','CA',775037.10,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Demo Company','Columbus','OH',746475.41,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Smith and Co.','Honolulu','HI',360465.90,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Foo Bars','Fresno','CA',399128.57,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('ABC Telecom','Birmingham','AL',311490.83,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Fake Brothers','Stockton','CA',312261.91,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('QWERTY Logistics','Lubbock','TX',172269.05,1);
+Insert Customer (Name, City, State, Sales, Active) Values ('Demo, Inc.','Las Vegas','NV',695515.57,1);
 Insert into Customer (Name, City, State, Sales, Active) Values 
 ('Inconspicuous Company','Cincinnati','OH',999999999999999999.99,0),
 ('Unassuming Inc','Lexington','KY',3.50,1);
