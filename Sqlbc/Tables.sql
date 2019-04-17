40ec5243e75cd1fcca4782bc5f90ca3d3291bc31
Chase <chasewilliams98@gmail.com>
2019-04-16 23:23:37 -0400

    added assignment

---
+++ Sqlbc/Tables.sql
@@ -0,0 +1,2 @@
+SELECT * FROM customer;
+SELECT c.Name, c.City, s.Name FROM Customer c JOIN State s ON c.State = s.Code;
---
+++ Sqlbc/costumer.sql
@@ -0,0 +1,3 @@
+SELECT * FROM customer WHERE Sales < 500000 AND Sales > 1000 ORDER BY Sales DESC;
+
+SELECT Name, Sales as 'Total  n Sales' FROM customer;
